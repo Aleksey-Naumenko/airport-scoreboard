135e0d1fe7cd7e73e0d7a089010f856bd5e376cc
Aleksey <aleksey_naumenko@ukr.net>
2020-03-23 10:53:47 +0200

    Updates

---
+++ precache-manifest.726d883aeecf4f932ce722d4251c810c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c97480a75482297e63e53c70ce75bf",
+    "url": "/airport-scoreboard/index.html"
+  },
+  {
+    "revision": "aa5cee11082fac337c2a",
+    "url": "/airport-scoreboard/static/css/main.bd30b6d7.chunk.css"
+  },
+  {
+    "revision": "58bcd8d41e673529165c",
+    "url": "/airport-scoreboard/static/js/2.35781aff.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/airport-scoreboard/static/js/2.35781aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5cee11082fac337c2a",
+    "url": "/airport-scoreboard/static/js/main.8c83f684.chunk.js"
+  },
+  {
+    "revision": "969d4c251864e9a2013f",
+    "url": "/airport-scoreboard/static/js/runtime-main.e603d922.js"
+  },
+  {
+    "revision": "2d2c20bb17425790a88a91a915de33d3",
+    "url": "/airport-scoreboard/static/media/logo-airport.2d2c20bb.png"
+  }
+]);
